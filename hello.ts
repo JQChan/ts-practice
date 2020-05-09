1c692d69d869a1235a388bbe928926ee1d7a4322
JQ_Chan <vikers28@qq.com>
2020-05-09 09:35:36 +0800

    泛型

--- hello.ts
+++ hello.ts
@@ -1,1 +1,21 @@
 console.log('hello world')
+
+/**
+ * 泛型
+ * @param arg 
+ */
+function hello<T>(arg: T): T {
+  return arg;
+}
+
+/**
+ * 使用熟悉的尖括号方式进行表达
+ */
+let output = hello<string>("hello TypeScript");
+
+/**
+ * 使用类型推断
+ */
+let output1 = hello("hello TypeScript");
+
+console.log(output, output1);
